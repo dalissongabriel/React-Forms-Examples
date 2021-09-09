b163e0a652e7679b143ee44220ab27f73b32ae07
dalissongabriel <alissongabrieldino@gmail.com>
2021-09-08 23:12:00 -0300

    feat: stylized h1 inside container component

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -9,4 +9,11 @@ export const Container = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  h1 {
+    color: var(--titulos);
+    font-family: 'Dancing Script';
+    font-size: 5rem;
+    margin-bottom: 2rem;
+  }
 `
