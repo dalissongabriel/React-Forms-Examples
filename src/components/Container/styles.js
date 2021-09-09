bf8aa9df9aeeb395b5e66c09cbbd4841e4bfb0a1
dalissongabriel <alissongabrieldino@gmail.com>
2021-09-08 22:54:27 -0300

    fix: flex direction switch to column in container

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -6,6 +6,7 @@ export const Container = styled.main`
   background-color: var(--background);
   color: var(--textos);
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `
